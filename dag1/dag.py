c2d15ed12cdfe2bb5b741c1910538935963fb427
dev@imwiz.com <cloud_user@3f9d1063981c.mylabserver.com>
2020-12-23 22:06:08 +0000

    more

---
+++ dag1/dag.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import dask.delayed as delayed
+from dask.diagnostics import ProgressBar
+
+
+def add_two(x):
+  return x + 2
+
+def sum_two_numbers(x,y):
+  return x + y
+
+def multiply_four(x):
+  return x * 4
+
+def main():
+    print("dag-1")
+    data = [1,5,8,10]
+    step1 = [delayed(add_two)(i) for i in data]
+    step2 = [delayed(multiply_four)(j) for j in step1]
+    total = delayed(sum)(step2)
+    total.visualize()
+
+if __name__ == "__main__":
+    main()
--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -4,14 +4,18 @@ import pandas as pd
 import dask.dataframe as dd
 from dask.diagnostics import ProgressBar
 from matplotlib import pyplot as plt
+import asyncio
+#from dask.distributed import Scheduler, Worker, Client
+
+#client = Client()
 
 
-df = dd.read_csv('../data/nyc-parking-tickets/*2017.csv')
+df = dd.read_csv('../data/nyc-parking-tickets/*filtered_2.csv')
 
 # Type conversions
-df["Date.First.Observed"] = pd.to_datetime(df["Date.First.Observed"])
-df["Vehichle.Expiration.Date"] = pd.to_datetime(df["Vehicle.Expiration.Date"])
-df["NTA"] = pd.to_object(df["NTA"])
+#df["Date.First.Observed"] = pd.to_datetime(df["Date.First.Observed"])
+#df["Vehichle.Expiration.Date"] = pd.to_datetime(df["Vehicle.Expiration.Date"])
+#df["NTA"] = pd.to_object(df["NTA"])
 
 print(F"Imported data types: {df.dtypes}")
 
@@ -24,5 +28,17 @@ missing_count = ((missing_values / df.index.size) * 100)
 print(F"Missing count: {missing_count}")
 
 with ProgressBar():
+  # This is now a Pandas Series object
   missing_count_pct = missing_count.compute()
 print(F"missing count pct {missing_count_pct}")
+
+columns_to_drop = missing_count_pct [missing_count_pct > 60].index
+print(F"Columns to drop: {columns_to_drop}")
+with ProgressBar():
+  df_dropped = df.drop(labels=columns_to_drop, axis=1)
+ # df = df.set_index('timestamp')
+ # client.persist(df_dropped)
+print(F"df_dropped = {df_dropped}")
+
+
+
